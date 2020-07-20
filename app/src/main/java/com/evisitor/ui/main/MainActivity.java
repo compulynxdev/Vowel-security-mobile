cb938b703360e9ed2a06123feb244d4679c7b9e0
Hemant <imhsharma1@gmail.com>
2020-07-20 12:13:24 +0530

    Dashboard Setup

--- app/src/main/java/com/evisitor/ui/main/MainActivity.java
+++ app/src/main/java/com/evisitor/ui/main/MainActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.evisitor.R;
 import com.evisitor.ViewModelProviderFactory;
 import com.evisitor.databinding.ActivityMainBinding;
 import com.evisitor.ui.base.BaseActivity;
 import com.evisitor.ui.base.BaseNavigator;
 
-public class MainActivity extends BaseActivity<ActivityMainBinding,MainViewModel> implements BaseNavigator {
+
+public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewModel> implements BaseNavigator {
+
+    private final static int ID_HOME = 1;
+    private final static int ID_EXPLORE = 2;
+    private final static int ID_SETTINGS = 3;
+    private final static int ID_PROFILE = 4;
 
     @Override
     public int getBindingVariable() {
@@ -31,5 +38,35 @@ public class MainActivity extends BaseActivity<ActivityMainBinding,MainViewModel
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getViewModel().setNavigator(this);
+
+        getViewDataBinding().bottomNavigation.add(new MeowBottomNavigation.Model(ID_HOME, R.drawable.ic_home));
+        getViewDataBinding().bottomNavigation.add(new MeowBottomNavigation.Model(ID_EXPLORE, R.drawable.ic_explore));
+        getViewDataBinding().bottomNavigation.add(new MeowBottomNavigation.Model(ID_SETTINGS, R.drawable.ic_settings));
+        getViewDataBinding().bottomNavigation.add(new MeowBottomNavigation.Model(ID_PROFILE, R.drawable.ic_profile));
+
+        getViewDataBinding().bottomNavigation.setOnClickMenuListener(item -> {
+        });
+        getViewDataBinding().bottomNavigation.setOnReselectListener(item -> {
+        });
+        getViewDataBinding().bottomNavigation.setOnShowListener(item -> {
+
+            String name = "";
+            switch (item.getId()) {
+                case ID_HOME:
+                    name = "Home";
+                    break;
+                case ID_EXPLORE:
+                    name = "Explore";
+                    break;
+                case ID_SETTINGS:
+                    name = "Settings";
+                    break;
+                case ID_PROFILE:
+                    name = "Profile";
+                    break;
+            }
+            getViewDataBinding().tvTitle.setText(name);
+        });
+        getViewDataBinding().bottomNavigation.show(ID_HOME, true);
     }
 }
