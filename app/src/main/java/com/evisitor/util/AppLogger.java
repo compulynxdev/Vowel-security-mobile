ef73a924ab0bf9cbd3231afe6560b0e1962f6f5c
Hemant <imhsharma1@gmail.com>
2021-02-24 12:20:45 +0530

    V2.0 production version

--- app/src/main/java/com/evisitor/util/AppLogger.java
+++ app/src/main/java/com/evisitor/util/AppLogger.java
@@ -1,7 +1,8 @@
 package com.evisitor.util;
 
-
 import android.util.Log;
+
+import com.evisitor.BuildConfig;
 
 /**
  * Created by Hemant Sharma on 23-02-20.
@@ -13,28 +14,34 @@ public final class AppLogger {
         // This class is not publicly instantiable
     }
 
-    public static void debug(String msg) {
-        Log.d("Debug", msg);
-    }
-
     public static void d(String tag, String msg) {
-        Log.d(tag, msg);
+        if (BuildConfig.DEBUG_MODE) {
+            Log.d(tag, msg);
+        }
     }
 
     public static void e(String tag, String msg) {
-        Log.e(tag, msg);
+        if (BuildConfig.DEBUG_MODE) {
+            Log.e(tag, msg);
+        }
     }
 
     public static void i(String tag, String msg) {
-        Log.i(tag, msg);
+        if (BuildConfig.DEBUG_MODE) {
+            Log.i(tag, msg);
+        }
     }
 
     public static void w(String tag, String msg) {
-        Log.w(tag, msg);
+        if (BuildConfig.DEBUG_MODE) {
+            Log.w(tag, msg);
+        }
     }
 
     public static void v(String tag, String msg) {
-        Log.v(tag, msg);
+        if (BuildConfig.DEBUG_MODE) {
+            Log.v(tag, msg);
+        }
     }
 
 }
