13fce9e4e8b8f2c2d1191bc78489585f27cf8d6a
faima <faima.mwami@compulynx.com>
2021-11-19 17:23:37 +0300

    corrected the conditional statement on line 300 CommercialAddVisitorActivity.java and 362 AddVisitorActivity.java

--- app/src/main/java/com/evisitor/ui/main/commercial/add/CommercialAddVisitorActivity.java
+++ app/src/main/java/com/evisitor/ui/main/commercial/add/CommercialAddVisitorActivity.java
@@ -297,7 +297,7 @@ public class CommercialAddVisitorActivity extends BaseActivity<ActivityCommercia
             @Override
             public void afterTextChanged(Editable s) {
                 if(!s.toString().isEmpty() && s.toString().length()>1){
-                    if (Double.parseDouble(s.toString()) >= 34 && Double.parseDouble(s.toString()) <= 40){
+                    if (Double.parseDouble(s.toString()) <= 34 || Double.parseDouble(s.toString()) >= 40){
                         getViewDataBinding().etTemperature.setText("");
                         showToast(R.string.temperature_should_be_30);
                     }
